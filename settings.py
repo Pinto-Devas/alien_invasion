9f784eb29cc5b5928108aead73f4af84021b03b8
Adevaldo Junior <adevaldopinto@hotmail.com>
2021-09-09 16:25:06 -0300

    :sparkles: Adding new features

--- settings.py
+++ settings.py
@@ -3,7 +3,10 @@ class Settings():
 
   def __init__(self):
     """Inicializa as configurações do jogo."""
-    #Configurações da tela
+    # Configurações da tela
     self.screen_width = 1200
     self.screen_height = 650
     self.bg_color = (230, 230, 230)
+
+    # Configurações da espaçonave
+    self.ship_speed_factor = 1.5
--- ship.py
+++ ship.py
@@ -1,18 +1,37 @@
 import pygame
 
 class Ship():
-  def __init__(self, screen):
+  def __init__(self, ai_settings, screen):
     """Inicializa a espaçonave e define sua posição inicial."""
     self.screen = screen
+    self.ai_settings = ai_settings
 
-    #Carrega a imagem da espaçonave e obtém seu rect
+    # Carrega a imagem da espaçonave e obtém seu rect
     self.image = pygame.image.load('images/ship.bmp')
     self.rect = self.image.get_rect()
     self.screen_rect = screen.get_rect()
 
-    #Inicializa cada nova espaçonave na parte inferior central da tela
+    # Inicializa cada nova espaçonave na parte inferior central da tela
     self.rect.centerx = self.screen_rect.centerx
     self.rect.bottom = self.screen_rect.bottom
+
+    # Armazena um valor decimal para o centro da espaçonave
+    self.center = float(self.rect.centerx)
+
+    # Flags de movimento
+    self.moving_right = False
+    self.moving_left = False
+
+  def update(self):
+    """Atualiza a posição da espaçonave de acordo com as flags de movimento."""
+    # Atualiza o valor do centro da espaçonave, e não o retângulo
+    if self.moving_right == True:
+      self.center += self.ai_settings.ship_speed_factor
+    if self.moving_left == True:
+      self.center -= self.ai_settings.ship_speed_factor
+    
+    # Atualiza o objeto rect de acordo com self.center
+    self.rect.centerx = self.center
 
   def blitme(self):
     """Desenha a espaçonave em sua posição atual."""
